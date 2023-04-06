2d5e63c6a851de499cbe1433876045317ba1d6b1
ChocolateHokage <pav.korotkov.03@gmail.com>
2023-04-06 10:55:22 +0300

    add: initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { postgresConfig } from './configs/postgres.config';
+import { AppConfigService } from './configs/app-config.service';
+import { AppConfigModule } from './configs/app-config.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ThrottlerModule.forRoot({
+      limit: 10,
+      ttl: 60,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      inject: [AppConfigService],
+      useFactory: postgresConfig,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
