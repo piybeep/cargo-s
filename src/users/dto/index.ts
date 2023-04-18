a9582e5aec9cd0c37cfc902a4aad8b8daa5f7532
ChocolateHokage <92468335+ChocolateHokage@users.noreply.github.com>
2023-04-18 11:42:31 +0300

    2-пользователи (#9)
    * add: initialization
    
    * add: users :))
    
    * fix: documentation
    
    * fix: response of user
    
    * fix: eslint
    
    * fix: cors settings
    
    * fix: format
    
    * fix: unused variables
    
    * fix: dependency
    
    * change: package manager
    
    * clear: logging
    
    * fix: doc
    
    * fix: doc
    
    ---------
    
    Signed-off-by: ChocolateHokage <92468335+ChocolateHokage@users.noreply.github.com>

---
+++ src/users/dto/index.ts
@@ -0,0 +1,4 @@
+import { CreateUserBodyDto } from './create-user-body.dto';
+import { UpdateUserDto } from './update-user.dto';
+
+export { CreateUserBodyDto, UpdateUserDto };
---
+++ src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { FindOptionsWhere, Repository } from 'typeorm';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserBodyDto } from './dto/create-user-body.dto';
+import { hashSync } from 'bcrypt';
+import { UpdateUserDto } from './dto';
+import { LoginUserResponseDto } from 'src/auth/dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async signUp(
+    userData: CreateUserBodyDto,
+  ): Promise<LoginUserResponseDto | undefined> {
+    userData.password = hashSync(userData.password, 10);
+    const user = this.userRepository.create(userData);
+    await this.userRepository.save(user);
+    delete user.password;
+    delete user.code;
+    return user;
+  }
+
+  async findOne(findParams: { email?: string; id?: string }): Promise<User> {
+    return await this.userRepository.findOneBy(findParams);
+  }
+
+  async update(findOptions: FindOptionsWhere<User>, data: UpdateUserDto) {
+    const updatedUser = await this.userRepository.update(findOptions, data);
+    if (updatedUser.affected !== 0) {
+      return await this.userRepository.findOneBy({
+        email: data.email || findOptions.email,
+      });
+    } else {
+      throw new InternalServerErrorException('Internal server error');
+    }
+  }
+}
