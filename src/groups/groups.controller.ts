12d524bc5a73adf4a963c41cb1377bd2cf257117
ChocolateHokage <pav.korotkov.03@gmail.com>
2023-04-20 13:53:33 +0300

    add: groups

---
+++ src/groups/groups.controller.ts
@@ -0,0 +1,109 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCookieAuth,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UpdateGroupDto } from './dto';
+import { GroupsService } from './groups.service';
+import { Group } from 'src/groups/entities';
+
+@ApiTags('Группы')
+@Controller('groups')
+export class GroupsController {
+  constructor(private readonly groupsService: GroupsService) {}
+@ApiCookieAuth('token')
+  @ApiOperation({
+    summary: 'Создание группы',
+  })
+  @ApiQuery({
+    name: 'projectId',
+    example: 'ada8c2c9-533d-4ed1-ba84-53282ad8cfef',
+    description: 'Id проекта',
+  })
+  @ApiResponse({ status: 201, type: Group })
+  @ApiResponse({ status: 400, description: 'BAD_REQUEST' })
+  @ApiResponse({ status: 500, description: 'INTERNAL_SERVER_ERROR' })
+  @Post()
+  async createGroup(@Query('projectId', ParseUUIDPipe) projectId: string) {
+    return await this.groupsService.createGroup(projectId);
+  }
+
+  @ApiOperation({
+    summary: 'Получение всех групп проекта, с поиском по наванию груза',
+  })
+  @ApiQuery({
+    name: 'projectId',
+    example: 'ada8c2c9-533d-4ed1-ba84-53282ad8cfef',
+    description: 'Id проекта',
+  })
+  @ApiQuery({
+    name: 'searchString',
+    example: 'груз 1',
+    description: 'Строка поиска',
+    required: false,
+  })
+  @ApiResponse({ status: 201, type: [Group] })
+  @ApiResponse({ status: 400, description: 'BAD_REQUEST' })
+  @ApiResponse({ status: 500, description: 'INTERNAL_SERVER_ERROR' })
+  @Get()
+  async getAllGroups(
+    @Query('projectId', ParseUUIDPipe) projectId: string,
+    @Query('searchString') searchString?: string,
+  ) {
+    return await this.groupsService.getAllGroups(projectId, searchString);
+  }
+
+  @ApiOperation({
+    summary: 'Обновление имени, видимости и позиции группы',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Id группы',
+    example: 'ada8c2c9-533d-4ed1-ba84-53282ad8cfef',
+  })
+  @ApiBody({
+    type: UpdateGroupDto,
+  })
+  @ApiResponse({ status: 200, type: Group })
+  @ApiResponse({ status: 400, description: 'BAD_REQUEST' })
+  @ApiResponse({ status: 500, description: 'INTERNAL_SERVER_ERROR' })
+  @Put(':id')
+  async updateGroup(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body()
+    data: UpdateGroupDto,
+  ) {
+    return await this.groupsService.updateGroup(id, data);
+  }
+
+  @ApiOperation({
+    summary: 'Удаление группы с ее грузами',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Id группы',
+    example: 'ada8c2c9-533d-4ed1-ba84-53282ad8cfef',
+  })
+  @ApiResponse({ status: 200, description: 'OK' })
+  @ApiResponse({ status: 400, description: 'BAD_REQUEST' })
+  @ApiResponse({ status: 500, description: 'INTERNAL_SERVER_ERROR' })
+  @Delete(':id')
+  async deleteGroup(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.groupsService.deleteGroup(id);
+  }
+}
